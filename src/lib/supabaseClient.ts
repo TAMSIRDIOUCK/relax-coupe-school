085077b4c38e5d21a7e8ec8b1ace18786f29b014
Tamsir Diouck <sirdiouck101@gmail.com>
2025-09-02 01:44:48 +0000

    Mise à jour App.tsx

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,12 +1,7 @@
+// src/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://vzhcjvvgpbtfolxnpapy.supabase.co'; // Remplacez par votre URL Supabase
-const supabaseKey = 'yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6aGNqdnZncGJ0Zm9seG5wYXB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyMDM2NjQsImV4cCI6MjA2ODc3OTY2NH0.8u-z9iou6prxy_yk1S_49-kFRwLCm8gTDiWAa18lS3g'; 
-
-export const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    persistSession: true,         // ✅ Garde la session active même après refresh
-    autoRefreshToken: true,       // ✅ Rafraîchit automatiquement les tokens expirés
-    detectSessionInUrl: true      // ✅ Gère les callbacks après connexion via lien magique
-  }
-});
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL!,
+  import.meta.env.VITE_SUPABASE_ANON_KEY!
+);
